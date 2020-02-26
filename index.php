6ee4a3b3d28d55689518c2c7310231a82e2fca0e
Minna <minna.petajaniemi@metropolia.fi>
2020-02-26 21:45:46 +0200

    update

--- index.php
+++ index.php
@@ -60,7 +60,6 @@ if (isset($_POST["submit"])) {
 
   if (!empty($_POST["numb1"]) and !empty($_POST["numb2"]) and !empty($_POST["numb3"])and !empty($_POST["numb4"])and !empty($_POST["numb5"])and !empty($_POST["numb6"])) {
 
-    echo $rand1 . $rand2 . $rand3 . $rand4 . $rand5 . $rand6;
 
     $postnum = array($_POST["numb1"],
     $_POST["numb2"],
@@ -70,20 +69,41 @@ if (isset($_POST["submit"])) {
     $_POST["numb6"]);
 
 
-    $randnum = array(rand(1,30),
-    rand(1,30),
-    rand(1,30),
-    rand(1,30),
-    rand(1,30),
-    rand(1,30),
+    $randnum = [];
+    $a = 1;
 
-  );
+     while ($a <= 6) {
+       $num = mt_rand(1,30);
+       if (!in_array($num, $randnum)) {
+         array_push ($randnum, $num);
+         $a++;// code...
+       }// code...
+     }
 
 
-    echo $postnum[0] . $postnum[1] . $postnum[2] . $postnum[3] . $postnum[4] . $postnum[5] . "<br>";
-    echo $randnum[0] . $randnum[1] . $randnum[2] . $randnum[3] . $randnum[4] . $randnum[5] . "<br>";
+     }
+     $count = 0;
+     foreach ($randnum as $key => $value) {
+         if(in_array(trim($value), $postnum))
+             $count++;
+
+
+     }
+     echo "Your numbers:" . " " . $postnum[0] . " " . $postnum[1] . " " . $postnum[2] . " " . $postnum[3] . " " . $postnum[4] . " " . $postnum[5] . "<br>";
+     echo "Winning numbers:" . " " . $randnum[0] . " " . $randnum[1] . " " . $randnum[2] . " " . $randnum[3] . " " . $randnum[4] . " " . $randnum[5] . "<br>";
+
+     if ($count == 0) {
+       echo "No correct numbers this time.";// code...
+     }
+     if ($count == 6) {
+       echo "Congratulations, you got all 6 right! You won the Lottery!";// code...
+     }
+     if ($count < 6 and $count > 0) {
+       echo "You got " . $count . " correct!"; // code...
+     }
 
 }
+
   else {
     echo "You must enter all the six numbers!";// code...
   }
